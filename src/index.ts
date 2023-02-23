3b5da46226824dcbedfff3b770cee5c81956aeb8
CN-traveler <cn-traveler@outlook.com>
2023-02-23 20:16:47 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import { Context, Schema } from 'koishi'
+import axios from 'axios'
 
 export const name = 'worker-proxy'
 
-export interface Config {}
+export interface Config {
+  url: string,
+  mode: boolean,
+  list: string[]
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  url: Schema.string().required().description('地址'),
+  mode: Schema.union([
+    Schema.const(true).description('白名单'),
+    Schema.const(false).description('黑名单')
+  ]).default(true).description('模式'),
+  list: Schema.array(String).role('table').description('域名列表'),
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(_ctx: Context, config: Config) {
+  axios.interceptors.request.use((value) => {
+    try {
+      if (config.list.includes(new URL(value.url).hostname) === config.mode) {
+        const url = new URL(config.url)
+        url.searchParams.set('url', value.url)
+        value.url = url.href
+      }
+    }
+    finally {
+      return value
+    }
+  })
 }
